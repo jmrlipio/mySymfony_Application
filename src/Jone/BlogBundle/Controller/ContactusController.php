428df4b1bb0ecb1539541b799e7b97d04b4aa520
Jone <jmrlipio@gmail.com>
2014-04-22 15:19:31 +0800

    Development Stage v1.1

--- src/Jone/BlogBundle/Controller/ContactusController.php
+++ src/Jone/BlogBundle/Controller/ContactusController.php
@@ -3,12 +3,40 @@
 namespace Jone\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Jone\BlogBundle\Entity\Enquiry;
+use Jone\BlogBundle\Form\EnquiryType;
 
 class ContactusController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('JoneBlogBundle:Contactus:contactus.html.twig');
+        $enquiry = new Enquiry();
+	    $form = $this->createForm(new EnquiryType(), $enquiry);
+	
+	    $request = $this->getRequest();
+			    if ($request->getMethod() == 'POST') {
+			        $form->bind($request); 
+			
+			    if ($form->isValid()) {
+		
+		         $message = \Swift_Message::newInstance()
+		            ->setSubject('Contact enquiry from symblog')
+		            ->setFrom('enquiries@symblog.co.uk')
+		            ->setTo($this->container->getParameter('Jone_blog.emails.contact_email'))
+		            ->setBody($this->renderView('JoneBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+		        $this->get('mailer')->send($message);
+		
+				$this->get('session')->getFlashBag()->add(
+					'blogger-notice',
+					'Your contact enquiry was successfully sent. Thank you!'
+					);
+		        // Redirect - This is important to prevent users re-posting
+		        // the form if they refresh the page
+		        return $this->render('JoneBlogBundle:Contactus:contactus.html.twig',array('form' => $form->createView()));
+		    }
+	    }	
+		return $this->render('JoneBlogBundle:Contactus:contactus.html.twig',array('form' => $form->createView()));
+        
       
 
         // render a PHP template instead
